6e845f43921bd7e27d2813e4e3c612588c110817
LOC4n <ngthanhson200696@gmail.com>
2025-11-17 23:13:42 +0700

    17/11

---
+++ Chuong4/Chuong4_BaiTap10.py
@@ -0,0 +1,77 @@
+#Câu 10: Vẽ hình dùng Sleep
+'''
+
+'''
+import time
+import os
+
+def xoa_man_hinh():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+hinh_1 = """
+       *
+      * *
+     * * *
+    * * * * *
+     * * *
+    * * * *
+    * *
+"""
+
+
+hinh_2 = """
+       *
+      * *
+     * * *
+    * * * * *
+     * * *
+    * *
+    *
+"""
+
+
+hinh_3 = """
+      * * *
+     * * *
+      * *
+       *
+      * *
+     * * *
+    * * * * *
+"""
+
+
+hinh_4 = """
+    * * * * *
+       * * *
+        * *
+         *
+        * *
+       * * *
+    * * * * *
+"""
+
+
+cac_hinh_ve = [hinh_1, hinh_2, hinh_3, hinh_4]
+
+
+
+
+for i, hinh in enumerate(cac_hinh_ve):
+ 
+    xoa_man_hinh()
+    
+
+    print(f"--- Hình {i + 1} ---")
+    
+  
+    print(hinh)
+    
+ 
+    print("\nChờ 5 giây để hiển thị hình tiếp theo...")
+    time.sleep(5)
+
+
+xoa_man_hinh()
+print("Hoàn thành!")
